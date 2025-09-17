0103740ae3c0f8c4e77b6e0837b6bd445ede88f1
NaNajli <nadurodriguez2@gmail.com>
2025-09-16 22:57:43 -0300

    W02 Code: Queues

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -7,23 +7,68 @@ public class PriorityQueueTests
 {
     [TestMethod]
     // Scenario: 
-    // Expected Result: 
+    // Expected Result: The Enqueue function shall add an item (which contains both data and priority) to the back of the queue.
     // Defect(s) Found: 
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("apple", 5);
+        var item = priorityQueue.Dequeue();
+        Assert.AreEqual("apple", item);
+
+        // Assert.Fail("Implement the test case and then remove this.");
     }
 
     [TestMethod]
     // Scenario: 
-    // Expected Result: 
+    // Expected Result: The Dequeue function shall remove the item with the highest priority and return its value.
     // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("apple", 5);
+        priorityQueue.Enqueue("mango", 8);
+        priorityQueue.Enqueue("mango", 2);
+        var item = priorityQueue.Dequeue();
+        Assert.AreEqual("mango", item);
+        // Assert.Fail("Implement the test case and then remove this.");
     }
 
     // Add more test cases as needed below.
+
+    [TestMethod]
+    // Scenario: 
+    // Expected Result:If there are more than one item with the highest priority, then the item closest to the front of the queue will be removed and its value returned.
+    // Defect(s) Found: 
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("apple", 8);
+        priorityQueue.Enqueue("mango", 8);
+        priorityQueue.Enqueue("potato", 2);
+        var item = priorityQueue.Dequeue();
+        Assert.AreEqual("mango", item);
+    }
+
+    [TestMethod]
+    // Scenario: 
+    // Expected Result:If the queue is empty, then an error exception shall be thrown. This exception should be an InvalidOperationException with a message of "The queue is empty."
+    // Defect(s) Found: 
+
+    public void TestPriorityQueue_4()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("tomate", 0);
+
+        try
+        {
+            priorityQueue.Dequeue();
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+            throw;
+        }
+    }
+
 }
--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -2,8 +2,10 @@
 /// Maintain a Customer Service Queue.  Allows new customers to be 
 /// added and allows customers to be serviced.
 /// </summary>
-public class CustomerService {
-    public static void Run() {
+public class CustomerService
+{
+    public static void Run()
+    {
         // Example code to see what's in the customer service queue:
         // var cs = new CustomerService(10);
         // Console.WriteLine(cs);
@@ -14,7 +16,9 @@ public class CustomerService {
         // Scenario: 
         // Expected Result: 
         Console.WriteLine("Test 1");
-
+        var service = new CustomerService(4);
+        service.AddNewCustomer();
+        service.ServeCustomer();
         // Defect(s) Found: 
 
         Console.WriteLine("=================");
@@ -22,6 +26,13 @@ public class CustomerService {
         // Test 2
         // Scenario: 
         // Expected Result: 
+        service = new CustomerService(4);
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        Console.WriteLine($"Before serving customers: {service}");
+        service.ServeCustomer();
+        service.ServeCustomer();
+        Console.WriteLine($"After serving customers: {service}");
         Console.WriteLine("Test 2");
 
         // Defect(s) Found: 
@@ -29,12 +40,39 @@ public class CustomerService {
         Console.WriteLine("=================");
 
         // Add more Test Cases As Needed Below
+
+        // test 3 
+        Console.WriteLine("Test 3");
+        service = new CustomerService(8);
+        service.ServeCustomer();
+        // Defect(s) Found: This found that I need to check the length in serve_customer and display an error message
+
+        Console.WriteLine("=================");
+
+        // test 4
+
+        Console.WriteLine("Test 4");
+        service = new CustomerService(4);
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        Console.WriteLine($"Service Queue: {service}");
+
+        // test 5
+        Console.WriteLine("Test 5");
+        service = new CustomerService(0);
+        Console.WriteLine($"Size should be 10: {service}");
+
+
     }
 
     private readonly List<Customer> _queue = new();
     private readonly int _maxSize;
 
-    public CustomerService(int maxSize) {
+    public CustomerService(int maxSize)
+    {
         if (maxSize <= 0)
             _maxSize = 10;
         else
@@ -45,8 +83,10 @@ public class CustomerService {
     /// Defines a Customer record for the service queue.
     /// This is an inner class.  Its real name is CustomerService.Customer
     /// </summary>
-    private class Customer {
-        public Customer(string name, string accountId, string problem) {
+    private class Customer
+    {
+        public Customer(string name, string accountId, string problem)
+        {
             Name = name;
             AccountId = accountId;
             Problem = problem;
@@ -56,7 +96,8 @@ public class CustomerService {
         private string AccountId { get; }
         private string Problem { get; }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return $"{Name} ({AccountId})  : {Problem}";
         }
     }
@@ -65,9 +106,11 @@ public class CustomerService {
     /// Prompt the user for the customer and problem information.  Put the 
     /// new record into the queue.
     /// </summary>
-    private void AddNewCustomer() {
+    private void AddNewCustomer()
+    {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count >= _maxSize)
+        {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
@@ -87,12 +130,19 @@ public class CustomerService {
     /// <summary>
     /// Dequeue the next customer and display the information.
     /// </summary>
-    private void ServeCustomer() {
-        _queue.RemoveAt(0);
-        var customer = _queue[0];
-        Console.WriteLine(customer);
+    private void ServeCustomer()
+    {
+        if (_queue.Count <= 0)
+        {
+            Console.WriteLine("No Customers in the queue");
+        }
+        else
+        {
+            var customer = _queue[0];
+            _queue.RemoveAt(0);
+            Console.WriteLine(customer);
+        }
     }
-
     /// <summary>
     /// Support the WriteLine function to provide a string representation of the
     /// customer service queue object. This is useful for debugging. If you have a 
@@ -100,7 +150,8 @@ public class CustomerService {
     /// see the contents.
     /// </summary>
     /// <returns>A string representation of the queue</returns>
-    public override string ToString() {
+    public override string ToString()
+    {
         return $"[size={_queue.Count} max_size={_maxSize} => " + string.Join(", ", _queue) + "]";
     }
 }
